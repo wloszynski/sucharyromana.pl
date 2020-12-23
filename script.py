beb560454d02e3a784935d71ff0092e9d6395c87
wloszynski <adrian@wloszynski.pl>
2020-12-23 17:40:35 +0100

    script for splitting jokes

--- script.py
+++ script.py
@@ -1,15 +1,27 @@
-sucharki = []
+# sucharki = []
 
-with open("suchary.txt", "r") as fp:
+# with open("suchary.txt", "r") as fp:
+#     Lines = fp.readlines()
+#     for line in Lines:
+#         sucharki.append(line)
+
+
+# sucharki_rev = sucharki[::-1]
+
+
+# with open("suchary_sorted.txt", "w") as fp:
+#     for such in sucharki_rev:
+#         fp.write(such)
+
+questions = []
+answers = []
+
+with open("suchary_sorted.txt", "r") as fp:
     Lines = fp.readlines()
     for line in Lines:
-        sucharki.append(line)
+        sucharek = line.split(';')
+        questions.append(sucharek[0])
+        answers.append(sucharek[1].replace('\n', ''))
 
-
-sucharki_rev = sucharki[::-1]
-
-
-with open("suchary_sorted.txt", "w") as fp:
-    for such in sucharki_rev:
-        fp.write(such)
-
+# print(questions)
+print(answers)
